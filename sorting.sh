c14be507004a107b7705fb2491d4766a164c7d5c
Tejasri Vanapalli <tejasri.v19@gmail.com>
2021-03-16 23:14:06 +0530

    [teju] modified sorting.sh file

--- sorting.sh
+++ sorting.sh
@@ -6,4 +6,9 @@ sum1=$(( $a+$b*$c ))
 sum2=$(( $a*$b+$c ))
 sum3=$(( $c+$a/$b ))
 sum4=$(( $a%$b+$c ))
+declare -A numbers
+numbers[sum1]="$sum1"
+numbers[sum2]="$sum2"
+numbers[sum3]="$sum3"
+numbers[sum4]="$sum4"
 
